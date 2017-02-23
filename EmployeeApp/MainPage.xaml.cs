0658b81ac7e6a8af7e304da6f18268492403bf6e
K8920 <K8920@OHJE-330-WS-35>
2017-02-23 14:46:20 +0200

    Demo07

--- EmployeeApp/MainPage.xaml.cs
+++ EmployeeApp/MainPage.xaml.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -15,7 +17,7 @@ using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace LayoutGrid
+namespace EmployeeApp
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
@@ -25,11 +27,22 @@ namespace LayoutGrid
         public MainPage()
         {
             this.InitializeComponent();
+            // change default startup mode
+            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+            //size 800x600
+            ApplicationView.PreferredLaunchViewSize = new Windows.Foundation.Size(800, 600);
+            // disable debugging info
+            App.Current.DebugSettings.EnableFrameRateCounter = false;
         }
 
-        private void TextBlock_SelectionChanged(object sender, RoutedEventArgs e)
+        private void saveButton_Click(object sender, RoutedEventArgs e)
         {
-
+            Debug.WriteLine("Name:" + nameTextBox.Text);
+            Debug.WriteLine("Email:" + emailTextBox.Text);
+            if ((bool)normalUserCheckBox.IsChecked)
+            {
+                Debug.WriteLine("Normal User");
+            }
         }
     }
 }
